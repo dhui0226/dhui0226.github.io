fff1d44e8b66e728d46d77130aad1827e86b21cf
dhui <dhui0226@gmail.com>
2021-08-09 22:28:08 -0500

    nav bar

--- src/index.js
+++ src/index.js
@@ -1,9 +1,11 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import { Nav } from './components'
 
 const App = () => {
     return (
         <div>
+            <Nav />
             <h1>David Hui</h1>
             <h2>I make stuff</h2>
             <div>Full Stack Developer based in Chicago.</div>
