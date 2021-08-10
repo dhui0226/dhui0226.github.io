cbd17fe4d4bb3d969fd7712cb92819d622f409d2
dhui <dhui0226@gmail.com>
2021-08-09 19:20:02 -0500

    tiny bit of description

--- src/index.js
+++ src/index.js
@@ -4,7 +4,9 @@ import ReactDOM from 'react-dom'
 const App = () => {
     return (
         <div>
-            <h1>index.js</h1>
+            <h1>David Hui</h1>
+            <h2>I make stuff</h2>
+            <div>Full Stack Developer based in Chicago.</div>
         </div>
     )
 }
