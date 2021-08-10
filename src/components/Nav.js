aa4a2488a27d51fc114354b62a84c72fbb9081a2
dhui <dhui0226@gmail.com>
2021-08-09 23:02:30 -0500

    icons for links

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,6 +1,8 @@
 import { Route, Switch, Link } from 'react-router-dom'
 import { default as About } from './About'
 import { default as Projects } from './Projects'
+import GitHubIcon from '@material-ui/icons/GitHub';
+import LinkedInIcon from '@material-ui/icons/LinkedIn';
 
 const Nav = () => {
     return (
@@ -9,6 +11,8 @@ const Nav = () => {
             <Link to='/'>Home</Link>
             <Link to='/about'>About</Link>
             <Link to='/projects'>Projects</Link>
+            <a href='https://github.com/dhui0226' target="_blank"><GitHubIcon /></a>
+            <a href='https://www.linkedin.com/in/david-hui-81b783215/' target="_blank"><LinkedInIcon /></a>
 
             <Switch>
                 <Route exact path='/'></Route>
