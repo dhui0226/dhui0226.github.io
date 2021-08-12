499546d430bca0588e15459595cd9e18beb061e1
dhui <dhui0226@gmail.com>
2021-08-12 00:20:39 -0500

    Home component

---
+++ src/components/Home.js
@@ -0,0 +1,15 @@
+import './Home.css'
+
+const Home = () => {
+    return (
+        <div>
+            <div className='homeContainer'>
+                <h1>David Hui</h1>
+                <h2>I make stuff</h2>
+                <div className='desc'>Full Stack Developer based in Chicago.</div>
+            </div>
+        </div>
+    )
+}
+
+export default Home
--- src/index.js
+++ src/index.js
@@ -1,24 +1,18 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
-import { Nav, About, Projects, Footer } from './components'
+import { Nav, Home, About, Projects, Footer } from './components'
 import './style.css'
 
 const App = () => {
     return (
-        <div className='homeContainer'>
+        <div>
             <Nav />
-            <div className='infoContainer'>
-                <h1>David Hui</h1>
-                <h2>I make stuff</h2>
-                <div>Full Stack Developer based in Chicago.</div>
             <Switch>
-                <Route exact path='/'></Route>
+                <Route exact path='/' component={Home}></Route>
                 <Route exact path='/about' component={About}></Route>
                 <Route exact path='/projects' component={Projects}></Route>
             </Switch>
-            </div>
-
             <Footer />
         </div>
     )
