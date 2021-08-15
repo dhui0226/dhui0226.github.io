b29318b29b42c81e63bb53cf2f009b5e41631680
dhui <dhui0226@gmail.com>
2021-08-14 19:33:48 -0500

    about section rough draft/filler/

--- src/components/About.js
+++ src/components/About.js
@@ -4,6 +4,11 @@ const About = () => {
     return (
         <div className='aboutContainer'>
             <h1>About</h1>
+            <div className='aboutDesc'>
+                <p>I'm always looking to add more to my skillset as well as improve my current skills.</p>
+                <p>I like powerlifting and cooking homemade food in my free time.</p>
+                <p>I like to refine my technique in whatever activities that interest me.</p>
+            </div>
         </div>
     )
 }
