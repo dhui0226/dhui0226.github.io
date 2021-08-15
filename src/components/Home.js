4e631be65a031178f3c27695b76128cb2ec531c9
dhui <dhui0226@gmail.com>
2021-08-14 19:48:03 -0500

    added image

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,9 +1,11 @@
 import './Home.css'
+import Image from './images/me.jpg'
 
 const Home = () => {
     return (
         <div>
             <div className='homeContainer'>
+                <img src={Image} alt='David Hui'/>
                 <h1>David Hui</h1>
                 <h2>I make stuff</h2>
                 <div className='desc'>I'm a Software Engineer based in <b>Chicago</b>.</div>
