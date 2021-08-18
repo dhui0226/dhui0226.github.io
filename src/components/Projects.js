b90b0b6e3bb4b9c940101035f907f34c07a7c94b
dhui <dhui0226@gmail.com>
2021-08-17 22:02:59 -0500

    spaced out project section

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -4,6 +4,12 @@ const Projects = () => {
     return (
         <div className='projectsContainer'>
             <h1>Projects</h1>
+            <div className='projectList'>
+                <div className='projectOne'></div>
+                <div className='projectTwo'></div>
+                <div className='projectThree'></div>
+                <div className='projectFour'></div>
+            </div>
         </div>
     )
 }
