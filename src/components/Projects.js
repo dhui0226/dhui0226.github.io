6b21a3f7d4993d38b811ed5021e88ca613088cde
dhui <dhui0226@gmail.com>
2021-08-19 11:43:38 -0500

    added discord bot to Projects

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,11 +1,16 @@
 import './Projects.css'
+import Image from './images/discordLogo.svg'
 
 const Projects = () => {
     return (
         <div className='projectsContainer'>
             <h1>Projects</h1>
             <div className='projectList'>
-                <div className='projectOne'></div>
+                <div className='projectOne'>
+                    <a href='https://github.com/dhui0226/discord_bot' target='_blank'>
+                        <img src={Image} alt='discord'/>
+                    </a>
+                </div>
                 <div className='projectTwo'></div>
                 <div className='projectThree'></div>
                 <div className='projectFour'></div>
