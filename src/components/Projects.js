2fc522ff27245afcebee2c1abc388605eedd24e4
dhui <dhui0226@gmail.com>
2021-08-19 13:56:06 -0500

    discord project

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -10,6 +10,25 @@ const Projects = () => {
                     <a href='https://github.com/dhui0226/discord_bot' target='_blank'>
                         <img src={Image} alt='discord'/>
                     </a>
+                    <div className='poContainer'>
+                        <a href='https://github.com/dhui0226/discord_bot' target='_blank'>
+                            <h2>Discord Bot</h2>
+                        </a>
+                        <a href='https://github.com/dhui0226/discord_bot' target='_blank'>
+                            <button>
+                                View Project
+                            </button>
+                        </a>
+                    </div>
+                    <div>
+                       <p>a bot for discord that helps you track your watchlist of crytocurrencies</p>
+                        <ul className='list'>
+                            <li>discord.js</li>
+                            <li>node js</li>
+                            <li>coingecko API</li>
+                            <li>postgresql</li>
+                        </ul>
+                    </div>
                 </div>
                 <div className='projectTwo'></div>
                 <div className='projectThree'></div>
