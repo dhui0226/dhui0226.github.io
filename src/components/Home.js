65d05f973c6804253b3099666e798c5daedc4dc7
dhui <dhui0226@gmail.com>
2021-09-12 23:57:02 -0500

    about me button

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,14 +1,22 @@
 import './Home.css'
+import { Link } from 'react-router-dom'
 import Image from './images/me.jpg'
 
 const Home = () => {
     return (
         <div>
             <div className='homeContainer'>
-                <img src={Image} alt='David Hui'/>
-                <h1>David Hui</h1>
-                <h2>Software Engineer</h2>
-                <p>I'm always looking to add more to my skillset as well as improve my current skills. I'm enthusastic about learning to write cleaner, reuseable and scalable code.</p>
+                <div className='homeItems'>
+                    <img src={Image} alt='David Hui'/>
+                    <h1>David Hui</h1>
+                    <h2>Software Engineer</h2>
+                    <p>I'm always looking to add more to my skillset as well as improve my current skills. I'm enthusastic about learning to write cleaner, reuseable and scalable code.</p>
+                    <Link to='/about'>
+                        <button className='aboutBtn'>
+                            More About Me
+                        </button>
+                    </Link>
+                </div>
             </div>
         </div>
     )
